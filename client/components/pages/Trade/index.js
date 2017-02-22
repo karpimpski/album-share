3330e93fd01489866d752e9101bea4a4a520c3c9
Michael Karpinski <mike.karpinski99@gmail.com>
2017-02-22 14:16:52 -0500

    trying to add multiple books per trade

--- client/components/pages/Trade/index.js
+++ client/components/pages/Trade/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router';
 import Header from '../../partials/Header';
 import Client from '../../Client.js';
+import './index.scss';
 
 class Trade extends React.Component {
 
@@ -9,7 +10,9 @@ class Trade extends React.Component {
 		super(props);
 		this.state = {
       self: {albums: []}, 
-      other: {albums: []}, 
+      other: {albums: []},
+      offeringNumber: 2,
+      requestingNumber: 1
     }
 	}
 
@@ -24,34 +27,80 @@ class Trade extends React.Component {
     });
   }
 
+  addOffering(){
+    this.setState({offeringNumber: this.state.offeringNumber + 1});
+  }
+
+  addRequest(){
+    this.setState({requestingNumber: this.state.requestingNumber + 1});
+  }
+
+  removeOffering(){
+    if(this.state.offeringNumber > 1){
+      this.setState({offeringNUmber: this.state.offeringNumber - 1});
+    }
+  }
+
+  removeRequest(){
+    if(this.state.requestingNumber > 1){
+      this.setState({requestingNumber: this.state.requestingNumber - 1});
+    }
+  }
+
   render(){
+    let offeringTemplate = 
+    <select name='offering'>
+      {this.state.self.albums.map((value, i) => {
+        return (
+          <option key={i}>{value}</option>
+        )
+      })}
+    </select>
+
+    let requestingTemplate = 
+    <select name='requesting'>
+      {this.state.other.albums.map((value, i) => {
+        return (
+          <option key={i}>{value}</option>
+        )
+      })}
+    </select>
+    let offering = [];
+    let requesting = [];
+    for(var i = 0; i < this.state.offeringNumber; i++){
+      offering.push(offeringTemplate);
+    }
+    for(var j = 0; j < this.state.requestingNumber; j++){
+      requesting.push(requestingTemplate);
+    }
+
     return (
       <div>
         <Header />
         <form method='POST' action='/api/newtrade'>
-          <input name='requester' value={this.state.self.username} />
-          <input name='target' value={this.state.other.username} />
-          <label>
-            You are giving:
-            <select name='offering'>
-              {this.state.self.albums.map((value, i) => {
-                return (
-                  <option key={i}>{value}</option>
-                )
-              })}
-            </select>
-          </label>
-
-          <label>
-            You are getting:
-            <select name='requesting'>
-              {this.state.other.albums.map((value, i) => {
-                return (
-                  <option key={i}>{value}</option>
-                )
-              })}
-            </select>
-          </label>
+          <input name='requester' value={this.state.self.username} className='hidden'/>
+          <input name='target' value={this.state.other.username} className='hidden'/>
+          <p>Giving: </p>
+          {offering.map((off, index) => {
+            return (
+              <div>
+                {off}<br/>
+              </div>
+            );
+          })}
+          <button onClick={this.addOffering.bind(this)}>Give More</button>
+          <button onClick={this.removeOffering}>Give Less</button>
+          <br/><br/>
+          {requesting.map((request, x) => {
+            return (
+              <div key={x}>
+                {request}<br/>
+                <button onClick={this.addRequest}>Request More</button>
+                <button onClick={this.removeRequest}>Request Less</button>
+              </div>
+            )
+          })}
+          
 
           <input type="submit" value="Submit" />
         </form>
